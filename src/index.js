af6347a5c81dabbc4a7dafcea4ff34c613520320
Alia Taha <aliataha5538@gmail.com>
2022-07-06 10:15:39 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import {App} from './components'
+const root = ReactDOM.createRoot(document.getElementById('app'))
+root.render(<h1>Hello, world!</h1>);
