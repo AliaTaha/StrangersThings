91fdcd8986397432bca04cf17989bf5a11f1850c
Alia Taha <aliataha5538@gmail.com>
2022-07-12 16:39:59 -0500

    post office hour

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { userLogin } from "../api";
-import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const Login = () => {
@@ -15,40 +14,43 @@ const Login = () => {
 
   const handleSubmit = async (event) => {
     try {
-        event.preventDefault()
-        const token = await userLogin(username, password);
-        localStorage.setItem("token", token);
-        console.log(token);
-      }
-     catch (error) {
-       console.log(error)
+      event.preventDefault();
+      const token = await userLogin(username, password);
+      localStorage.setItem("token", token);
+      console.log(token);
+    } catch (error) {
+      console.log(error);
     }
-  }
-  return localStorage.getItem("token") ? (
-    <div>{`${username}`} is logged in</div>
-  ) : (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Username: </label>
-      <input
-        type="text"
-        value={username}
-        placeholder="enter a username"
-        onChange={({ target }) => setUsername(target.value)}
-      />
-      <div>
-        <label htmlFor="password">Password: </label>
-        <input
-          type="password"
-          value={password}
-          placeholder="enter a password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">Login</button>
-      <div>
-        <Link to="/login/register">Create Account</Link>
-      </div>
-    </form>
+  };
+  return (
+    <>
+      {localStorage.getItem("token") ? (
+        <div>{`${username}`} is logged in</div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            value={username}
+            placeholder="enter a username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+          <div>
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              value={password}
+              placeholder="enter a password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+          <div>
+            <Link to="/login/register">Create Account</Link>
+          </div>
+        </form>
+      )}
+    </>
   );
 };
 export default Login;
